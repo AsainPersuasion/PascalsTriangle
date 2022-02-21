d1f8c19321baa1310debcb46d8bc4e722a9afbbd
Kaden <kaden@pop-os.localdomain>
2022-02-21 21:34:03 +0000

    first working version

--- Program.cs
+++ Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Threading;
 
 class Program 
@@ -8,16 +8,14 @@ class Program
     static void Main(String[] args)
     {
         int previousRowSize = 0;
-        int[] previousTriangle = {0};
+        int[] previousTriangle = {};
         int rowSize = 0;
+        Console.WriteLine("Enter shit now");
         int userInput = Convert.ToInt32(Console.ReadLine());
-        int gapSize = Convert.ToInt32(Math.Ceiling(userInput*0.5f));
-        string space = " ";
-        string gap = "";
-        
+        string[] pascalsTriangle = new string[userInput];
 
-
-        for(int i = 0; i != Convert.ToInt32(userInput);i++)
+        Console.WriteLine("Running");
+        for(int i = 0; i != userInput;i++)
         {
 
             rowSize++;
@@ -39,47 +37,58 @@ class Program
                 {
                     break;
                 }
-               
-                //Console.WriteLine("repeate: " + k);
+
                 triangle[k] = previousTriangle[k]+previousTriangle[k-1];
             }
 
-
+            pascalsTriangle[i] = IntListWrite(triangle);
             previousRowSize = rowSize;
             previousTriangle = triangle;
 
-
-            for(int b=0; b <= gapSize; b++)
-            {
-                gap += space;
-        
-            }
-
-            
-            Console.WriteLine(gap+IntListWrite(triangle));
-            gapSize -= 1;
-            gap = "";
-
-
-
-
-
-
-       
         }
 
+        int lastRowLength = pascalsTriangle[pascalsTriangle.Length-1].Length;
+        string space = " ";
+  
+        int x = pascalsTriangle[userInput-1].Length;
+        for(int p=userInput-1;p>=0 ;p--)
+            {
+
+                while( x >= pascalsTriangle[p].Length)
+                {
+                    pascalsTriangle[p] = space+pascalsTriangle[p]+space;
     
-    static string IntListWrite(int[] a)
+                }
+        
+            }
+        Console.WriteLine(StringListWrite(pascalsTriangle));
+
+    
+    static string IntListWrite(int[] a )
     {   
         string result = "";
         for(int j = 0; j!=a.Length; j++)
         {
-            result += a[j] + " ";            
+            result += a[j]+" ";            
         }
 
         return result;
 
     }
+
+    static string StringListWrite(string[] a )
+    {   
+        string result = "";
+        for(int j = 0; j!=a.Length; j++)
+        {
+            result += a[j]+"\n";            
+        }
+
+        return result;
+
+    }
+
+
 
     }
 }
