a1963a0e4db0fc136111edd249a3de0379fd7a39
Kaden <kaden@pop-os.localdomain>
2022-02-23 17:27:25 +0000

    Final Commit

--- Program.cs
+++ Program.cs
@@ -7,37 +7,39 @@ class Program
 
     static void Main(String[] args)
     {
-        int previousRowSize = 0;
-        int[] previousTriangle = {};
-        int rowSize = 0;
-        Console.WriteLine("Enter shit now");
+        
+        
+        Console.WriteLine("Welome to Pascal's Triangle!");
+        Console.Write("Please enter the number of rows you would like to be displayed: ");
         int userInput = Convert.ToInt32(Console.ReadLine());
-        string[] pascalsTriangle = new string[userInput];
 
-        Console.WriteLine("Running");
+
+        int previousRowSize = 0;
+        int rowSize = 0;
+        string[] pascalsTriangle = new string[userInput];
+        int[] previousTriangle = {};
+
         for(int i = 0; i != userInput;i++)
         {
 
             rowSize++;
 
-
+            //create list for num of new row
             int[] triangle= new int[rowSize];
-            //Console.WriteLine("Triangle Array Lenght " + triangle.Length);
-
-
             int rowsLeft = rowSize;
+            //makes first and last digits 1
             triangle[0] = 1;
             triangle[triangle.Length-1] = 1;
-            //Console.WriteLine("triangle length: "+triangle.Length);
-            
+           
+            //does loop after first 2 rows are complete 
             for(int k=1; k <= rowsLeft && rowSize > 2; k++)
             {
-                
+                //if loop reaches the last digit it ends
                 if(triangle.Length-1 == k)
                 {
                     break;
                 }
-
+                //adds the current positoin and previous position of the previous rowes to gether to create new row
                 triangle[k] = previousTriangle[k]+previousTriangle[k-1];
             }
 
@@ -47,23 +49,25 @@ class Program
 
         }
 
-        int lastRowLength = pascalsTriangle[pascalsTriangle.Length-1].Length;
-        string space = " ";
-  
+
         int x = pascalsTriangle[userInput-1].Length;
+        //loop for every row
         for(int p=userInput-1;p>=0 ;p--)
             {
-
+                //loops for every digit in a row 
+                //add spaces untill length is equal to last row
                 while( x >= pascalsTriangle[p].Length)
                 {
-                    pascalsTriangle[p] = space+pascalsTriangle[p]+space;
+                    
+                    pascalsTriangle[p] = " "+pascalsTriangle[p]+" ";
     
                 }
         
             }
+        //displays the final product
         Console.WriteLine(StringListWrite(pascalsTriangle));
 
-    
+    //turns int list into a string 
     static string IntListWrite(int[] a )
     {   
         string result = "";
@@ -76,6 +80,7 @@ class Program
 
     }
 
+    //used to turn a string list into string 
     static string StringListWrite(string[] a )
     {   
         string result = "";
